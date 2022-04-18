aa0be7f1348f0c8ea1f860178178b1ddce0316af
Anurag-Bharati <anuragbharati26@gmail.com>
2022-04-18 20:11:28 +0545

    Edited variables

--- bread/settings.py
+++ bread/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13ea-103-163-182-46.ngrok.io", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["http://13ea-103-163-182-46.ngrok.io", "https://13ea-103-163-182-46.ngrok.io"]
 
 # Application definition
 
