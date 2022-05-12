6add18bdbecd4f2787c36473246b025f17f503f5
Anurag-Bharati <anuragbharati26@gmail.com>
2022-05-12 13:40:57 +0545

    Removed unused Imports

--- bread/urls.py
+++ bread/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
 from bread import views
 
 urlpatterns = [
--- bread/views.py
+++ bread/views.py
@@ -1,4 +1,3 @@
-from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from manage.models import Product, Staff, Customer, Order
